ca24790bdcdd9916a84f6d579dc5bbc261cc42be
Aleksa <lukicaleksa04@gmail.com>
2020-09-23 18:54:43 +0200

    Check date for work order

--- src/app/admin/work-order/add-work-order-dialog/add-work-order-dialog.component.ts
+++ src/app/admin/work-order/add-work-order-dialog/add-work-order-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-work-order-dialog',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddWorkOrderDialogComponent implements OnInit {
 
+
+  workOrderForm = new FormGroup({
+    date: new FormControl("", Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add() {
+    
+    var date = new Date();
+    var formDate = new Date();
+
+    formDate = this.workOrderForm.get("date").value;
+
+    var x = date.getDate()+date.getMonth()+date.getFullYear();
+    var y = formDate.getDate()+formDate.getMonth()+formDate.getFullYear();
+    
+
+    if(x === y){
+
+      console.log(true);
+      
+    }else{
+        console.log(false);
+        
+    }
+    
+      
+
   }
 
 }
